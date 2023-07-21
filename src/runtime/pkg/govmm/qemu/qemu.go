2a9a763468051b6bf9a7ee29799c97f8b597fb3a
Tizian Leonhardt <tizian.leonhardt@tum.de>
2023-07-21 16:14:57 +0200

    chore: some logging

--- src/runtime/pkg/govmm/qemu/qemu.go
+++ src/runtime/pkg/govmm/qemu/qemu.go
@@ -3174,6 +3174,8 @@ func LaunchCustomQemu(ctx context.Context, path string, params []string, fds []*
 		path = "qemu-system-x86_64"
 	}
 
+	logger.infof("launching %s with: %v", path, params)
+
 	/* #nosec */
 	cmd := exec.CommandContext(ctx, path, params...)
 	if len(fds) > 0 {
